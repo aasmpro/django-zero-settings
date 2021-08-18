86b4d4c5ae5fe2594744dd4e5e37934b85887a8f
Abolfazl Amiri <aa.smpro@gmail.com>
2021-08-19 00:13:56 +0430

    v0.1.3

--- zero_settings/__init__.py
+++ zero_settings/__init__.py
@@ -4,7 +4,7 @@ from .settings import ZeroSettings, register_reload
 
 VersionInfo = namedtuple("VersionInfo", ("major", "minor", "patch"))
 
-VERSION = VersionInfo(0, 1, 2)
+VERSION = VersionInfo(0, 1, 3)
 
 __version__ = "{0.major}.{0.minor}.{0.patch}".format(VERSION)
 __all__ = [ZeroSettings, register_reload]
