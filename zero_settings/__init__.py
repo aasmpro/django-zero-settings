fb20476e6a73fa069f65e0490fd8af6dcac04a46
Abolfazl Amiri <abolfazl.amiri@trustyou.net>
2022-09-21 16:24:50 +0200

    v0.1.12

--- zero_settings/__init__.py
+++ zero_settings/__init__.py
@@ -4,7 +4,7 @@ from .settings import ZeroSettings
 
 VersionInfo = namedtuple("VersionInfo", ("major", "minor", "patch"))
 
-VERSION = VersionInfo(0, 1, 11)
+VERSION = VersionInfo(0, 1, 12)
 
 __version__ = "{0.major}.{0.minor}.{0.patch}".format(VERSION)
 __all__ = [ZeroSettings]
